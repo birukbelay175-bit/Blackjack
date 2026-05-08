caa6d4bde6409700c031a0e87fe28ad082712c1e
birukbelaytafese <birukbelay175@gmail.com>
2026-05-07 21:21:46 -0500

    Main commit

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -8,50 +9,50 @@ public class Main {
 
         System.out.println("Welcome to Blackjack!");
 
-        System.out.print("Enter player 1 name: ");
-        String player1Name = scanner.nextLine();
+        System.out.print("How many players? ");
+        int numberOfPlayers = scanner.nextInt();
+        scanner.nextLine();
 
-        System.out.print("Enter player 2 name: ");
-        String player2Name = scanner.nextLine();
+        ArrayList<Player> players = new ArrayList<>();
 
-        Player player1 = new Player(player1Name);
-        Player player2 = new Player(player2Name);
+        for (int i = 1; i <= numberOfPlayers; i++) {
+            System.out.print("Enter player " + i + " name: ");
+            String name = scanner.nextLine();
+
+            Player player = new Player(name);
+            players.add(player);
+        }
 
         Deck deck = new Deck();
         deck.shuffle();
 
-        player1.getHand().addCard(deck.dealCard());
-        player1.getHand().addCard(deck.dealCard());
+        for (Player player : players) {
+            player.getHand().addCard(deck.dealCard());
+            player.getHand().addCard(deck.dealCard());
+        }
 
-        player2.getHand().addCard(deck.dealCard());
-        player2.getHand().addCard(deck.dealCard());
-
-        System.out.println();
-        System.out.println(player1.getName() + "'s hand: " + player1.getHand());
-        System.out.println("Score: " + player1.getHand().getScore());
+        Player winner = null;
+        int bestScore = 0;
 
         System.out.println();
 
-        System.out.println(player2.getName() + "'s hand: " + player2.getHand());
-        System.out.println("Score: " + player2.getHand().getScore());
+        for (Player player : players) {
+            int score = player.getHand().getScore();
 
-        int score1 = player1.getHand().getScore();
-        int score2 = player2.getHand().getScore();
+            System.out.println(player.getName() + "'s hand: " + player.getHand());
+            System.out.println("Score: " + score);
+            System.out.println();
 
-        System.out.println();
+            if (score <= 21 && score > bestScore) {
+                bestScore = score;
+                winner = player;
+            }
+        }
 
-        if (score1 > 21 && score2 > 21) {
-            System.out.println("Both players busted. No winner.");
-        } else if (score1 > 21) {
-            System.out.println(player2.getName() + " wins!");
-        } else if (score2 > 21) {
-            System.out.println(player1.getName() + " wins!");
-        } else if (score1 > score2) {
-            System.out.println(player1.getName() + " wins!");
-        } else if (score2 > score1) {
-            System.out.println(player2.getName() + " wins!");
+        if (winner == null) {
+            System.out.println("Everyone busted. No winner.");
         } else {
-            System.out.println("It's a tie!");
+            System.out.println(winner.getName() + " wins with " + bestScore + " points!");
         }
 
         scanner.close();
