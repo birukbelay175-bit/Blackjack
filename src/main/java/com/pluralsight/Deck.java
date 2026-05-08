2059bba71f2f97a473bd77435a560794aaf4c311
birukbelaytafese <birukbelay175@gmail.com>
2026-05-07 21:01:04 -0500

    Deck commit

---
+++ src/main/java/com/pluralsight/Deck.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+    private ArrayList<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] values = {
+                "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                "Jack", "Queen", "King", "Ace"
+        };
+
+        for (String suit : suits) {
+            for (String value : values) {
+                cards.add(new Card(suit, value));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card dealCard() {
+        return cards.remove(0);
+    }
+}
+
